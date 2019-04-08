3480905c6bcfd0ea4f463cfea89b16656daef157
Paul Cuciureanu <paul@cuciureanu.com>
2019-04-07 22:38:52 -0400

    feat: create a new index

--- data.js
+++ data.js
@@ -4,6 +4,7 @@ const elasticsearch = require('elasticsearch');
 const client = new elasticsearch.Client({
    hosts: [ 'http://localhost:9200']
 });
+
 // ping the client to be sure Elasticsearch is up
 client.ping({
      requestTimeout: 30000,
@@ -15,3 +16,14 @@ client.ping({
          console.log('Everything is ok');
      }
  });
+
+// create a new index called scotch.io-tutorial. If the index has already been created, this function fails safely
+client.indices.create({
+  index: 'scotch.io-tutorial'
+}, function(error, response, status) {
+  if (error) {
+      console.log(error);
+  } else {
+      console.log("created a new index", response);
+  }
+});

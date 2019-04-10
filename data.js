70347586908aa335c1693d54ee1f05d6a8f867a2
Paul Cuciureanu <paul@cuciureanu.com>
2019-04-09 22:08:28 -0400

    feat: add bulk upload of cities demo data

--- data.js
+++ data.js
@@ -28,16 +28,40 @@ client.indices.create({
   }
 });
 
-// add a data to the index that has already been created
-client.index({
-  index: 'scotch.io-tutorial',
-  id: '1',
-  type: 'cities_list',
-  body: {
-      "Key1": "Content for key one",
-      "Key2": "Content for key two",
-      "key3": "Content for key three",
-  }
-}, function(err, resp, status) {
-  console.log(resp);
-});
+// // add a data to the index that has already been created
+// client.index({
+//   index: 'scotch.io-tutorial',
+//   id: '1',
+//   type: 'cities_list',
+//   body: {
+//       "Key1": "Content for key one",
+//       "Key2": "Content for key two",
+//       "key3": "Content for key three",
+//   }
+// }, function(err, resp, status) {
+//   console.log(resp);
+// });
+
+const cities = require('./data/cities.json');
+// declare an empty array called bulk
+var bulk = [];
+//loop through each city and create and push two objects into the array in each loop
+//first object sends the index and type you will be saving the data as
+//second object is the data you want to index
+cities.forEach(city =>{
+   bulk.push({index:{ 
+                 _index:"scotch.io-tutorial", 
+                 _type:"cities_list",
+             }          
+         })
+  bulk.push(city)
+})
+
+//perform bulk indexing of the data passed
+client.bulk({body: bulk}, function( err, response  ){ 
+         if( err ){ 
+             console.log("Failed Bulk operation".red, err) 
+         } else { 
+             console.log("Successfully imported %s".green, cities.length); 
+         } 
+}); 
